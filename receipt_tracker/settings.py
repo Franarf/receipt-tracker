ed6392b0357d3843d14bfd517b854f76018bf749
FRAN <fran@FRANs-MacBook-Pro.local>
2025-12-17 19:36:06 +0100

    Added receipts app, URLs, and basic view

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(str(BASE_DIR / 'receipts'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    'receipts.apps.ReceiptsConfig',
 ]
 
 MIDDLEWARE = [
