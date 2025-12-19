224a2fc31a36930999680b0df6bcfd3517820129
Franarf <greenblue.web@gmail.com>
2025-12-19 16:05:45 +0100

    Added basic image upload functionality and media storage

--- receipts/urls.py
+++ receipts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="receipts-index"),
+    path("upload/", views.upload_receipt, name="upload-receipt"),
+    path("upload/success/", views.upload_success, name="upload-success"),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ReceiptUploadForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -6,3 +7,19 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Welcome to the Receipts app!")
 
+def upload_receipt(request):
+    if request.method == "POST":
+        form = ReceiptUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            receipt = form.save(commit=False)
+            receipt.user = request.user  # temporary until auth is added
+            receipt.save()
+            return redirect("upload-success")
+    else:
+        form = ReceiptUploadForm()
+
+    return render(request, "upload.html", {"form": form})
+
+
+def upload_success(request):
+    return render(request, "upload_success.html")
