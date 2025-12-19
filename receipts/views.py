82e81cd63bd9b398714ac2783c09412c04d06dd5
Franarf <greenblue.web@gmail.com>
2025-12-19 16:15:11 +0100

    Added receipt list page with images and details

--- receipts/views.py
+++ receipts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
-from .forms import ReceiptUploadForm
 from django.http import HttpResponse
+
+from .forms import ReceiptUploadForm
+from .models import Receipt
+
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to the Receipts app!")
+    # return HttpResponse("Welcome to the Receipts app!")
+    receipts = Receipt.objects.all().order_by("-date")
+    return render(request, "receipts_list.html", {"receipts": receipts})
 
 def upload_receipt(request):
     if request.method == "POST":
